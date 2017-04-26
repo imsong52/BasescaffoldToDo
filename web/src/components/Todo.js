9baad4cdee0a9b4e08e9d133e5b9892c0ba36ba7
zhou <zhou@adhdeMacBook-Pro.local>
2017-04-26 14:28:30 +0800

    完成demo

--- web/src/components/Todo.js
+++ web/src/components/Todo.js
@@ -5,79 +5,68 @@ class Todo extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-       todos: {
-        // key is creation date
-        't1': {text: 'Board the plane'},
-        't2': {text: 'Sleep'},
-        't3': {text: 'Try to finish coneference slides'},
-        't4': {text: 'Eat cheese and drink wine'},
-        't5': {text: 'Go around in Uber'},
-        't6': {text: 'Talk with conf attendees'},
-        't7': {text: 'Show Demo 1'},
-        't8': {text: 'Show Demo 2'},
-        't9': {text: 'Lament about the state of animation'},
-        't10': {text: 'Show Secret Demo'},
-        't11': {text: 'Go home'},
-      },
+     todos: [
+       
+        {key: 't1', data: {text: 'Board the plane', isDone: false}},
+        {key: 't2', data: {text: 'Sleep', isDone: false}},
+        {key: 't3', data: {text: 'Try to finish conference slides', isDone: false}},
+        {key: 't4', data: {text: 'Eat cheese and drink wine', isDone: false}},
+        {key: 't5', data: {text: 'Go around in Uber', isDone: false}},
+        {key: 't6', data: {text: 'Talk with conf attendees', isDone: false}},
+        {key: 't7', data: {text: 'Show Demo 1', isDone: false}},
+        {key: 't8', data: {text: 'Show Demo 2', isDone: false}},
+        {key: 't9', data: {text: 'Lament about the state of animation', isDone: false}},
+        {key: 't10', data: {text: 'Show Secret Demo', isDone: false}},
+        {key: 't11', data: {text: 'Go home', isDone: false}},
+      ],
       value: '',
-       items: [{key: 'a', size: 10}, {key: 'b', size: 20}, {key: 'c', size: 30}],
+      selected: 'all',
+      items: [{key: 'a', size: 10}, {key: 'b', size: 20}, {key: 'c', size: 30}],
     }
      this.handleChange = this.handleChange.bind(this);
       this.handleDestroy = this.handleDestroy.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+         this.getStyles = this.getStyles.bind(this);
   }
   componentWillMount() {
    // this.setState({value:'a'});
   }
   componentDidMount() {
     this.setState({
-      items: [{key: 'a', size: 10}, {key: 'b', size: 20}], // remove c.
+      items: [{key: 'a', size: 10}, {key: 'b', size: 20}], 
     });
   }
     willLeave() {
-    // triggered when c's gone. Keeping c until its width/height reach 0.
+    
     return {width: spring(0), height: spring(0)};
   }
    willEnter(date) {
-    return {
-      height: spring(0),
-      opacity: spring(2),
-      data: this.state.todos[date],
+     return {
+      height: 0,
+      opacity: 1,
     };
   }
-  //   getDefaultValue() {
-  //   const {todos} = this.state;
-  //   return Object.keys(todos)
-  //     .reduce((configs, date) => {
-  //       configs[date] = {
-  //         height: spring(0),
-  //         opacity: spring(1),
-  //         data: todos[date],
-  //       };
-  //       return configs;
-  //     }, {});
-  // }
-
-   getEndValue() {
+  getStyles (){
     const {todos, value} = this.state;
-   let temp=Object.keys(todos)
-      .filter(date => {
-        const todo = todos[date];
-        return todo.text.toUpperCase().indexOf(value.toUpperCase()) >= 0
-      })
-      .reduce((configs, date) => {
-        configs[date] = {
-          // height: spring(60, presets.wobbly),
-          // opacity: spring(1, presets.wobbly),
-         styles: {width: spring(60), opacity: spring(1)},
-          data: todos[date],
-          key:date
-        };
-        return configs;
-      }, {});
-      console.log(temp)
-    return temp;
+    return todos.filter(({data: {isDone, text}}) => {
+      return text.toUpperCase().indexOf(value.toUpperCase()) >= 0 
+       
+    })
+    .map((todo, i) => {
+      
+      return {
+        key:todo.key,
+        data:todo.data,
+        index:i,
+        style: {
+          height: spring(60, presets.gentle),
+          opacity: spring(1, presets.gentle),
+          
+        }
+      };
+    });
   }
+
   // getTestVaule(){
   //   let temp= 
   //    console.log(temp);
@@ -86,23 +75,23 @@ class Todo extends React.Component {
   handleChange(event) {
     this.setState({value: event.target.value});
   }
-  handleDestroy(index) {
-   
+  handleDestroy(key) {
+    console.log(key);
      const {todos} = this.state;
-    delete todos[index];
-    this.forceUpdate();
-    console.log(this.state);
+   
+     this.setState({todos: this.state.todos.filter((data) => data.key !== key)});
+     console.log(this.state);
   }
 
 handleSubmit(e) {
     e.preventDefault();
-    const {todos, value} = this.state;
-   
-    let tmp=todos.push(value);
-     console.log(tmp);
-    this.setState({
-         
-    });
+  
+    const newItem = {
+      key: 't' + Date.now(),
+      data: {text: this.state.value, isDone: false},
+    };
+    // append at head
+    this.setState({todos: [newItem].concat(this.state.todos)});
   }
   render() {
    const {todos, value} = this.state;
@@ -128,59 +117,29 @@ handleSubmit(e) {
         willEnter={this.willEnter}
        
        styles={
-          Object.keys(this.state.todos)
-      .filter(date => {
-        const todo = todos[date];
-        return todo.text.toUpperCase().indexOf(value.toUpperCase()) >= 0
-      })
-      .reduce((configs, date) => {
-        configs[date] = {
-          height: spring(60, presets.wobbly),
-          opacity: spring(1, presets.wobbly),
-          data: todos[date],
-        }
-        })
+        this.getStyles
        }>
-         {configs =>
+         {styles =>
          <ul className="todo-list">
-            {/*{
-              this.state.todos.map((text,index)=>{
-                   return(
-                    <li key={index}>
-                         <div className="view">
+            {styles.map(({key, style,data: {text}}) =>
+                   
+                  <li key={key} style={style} >
+                     <div className="view">
                         
                         <label>{text}</label>
                         <button
                           className="destroy"
-                         onClick={this.handleDestroy.bind(null, index)}
+                          onClick={this.handleDestroy.bind(null, key)}
                         />
                       </div>
-                    </li>
-                   )
-            })}*/}
+                  </li>
+                )}
            </ul>
         }
             
               </TransitionMotion>
         </section>
-    <TransitionMotion
-        willLeave={this.willLeave}
-        styles={
-         this.state.items.map(item => ({
-          key: item.key,
-          style: {width: item.size, height: item.size},
-        }))
-        }>
-        {interpolatedStyles =>
-          // first render: a, b, c. Second: still a, b, c! Only last one's a, b.
-          <div>
-            {interpolatedStyles.map(config => {
-              console.log(config.style);
-              return <div key={config.key} style={{width:config.style.width,height:config.style.height, border: '1px solid'}} />
-            })}
-          </div>
-        }
-      </TransitionMotion>
+   
         <footer className="footer">
           <span className="todo-count">
             <strong>
